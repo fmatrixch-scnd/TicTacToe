ff2befca82e35e2cb6051d81b0fba34a91e4a34e
fmatrixch-scnd <fmatrixch@gmail.com>
2025-07-24 13:21:35 -0400

    Add files via upload

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,14 +6,72 @@
 #include <string>
 using namespace std;
 
+enum Statements {
+	BEGIN,
+	PROCESSING,
+	ABS_WIN,
+	ABS_LOSE
+};
+
+enum PlayerStatus {
+	NORMAL,
+	WIN,
+	LOSE,
+	TIE
+};
+
 class TicTacToe {
 private:
 	string role;
 	string opposite_role;
-	int winning_games = 0;
+	Statements stm = BEGIN;
 public:
-	bool game() {
-		bool result = false;
+	PlayerStatus judge(vector<vector<string>> chequerboard,int moves) {
+		for (int x = 0;x < chequerboard.size();x++) {
+			if (chequerboard[x][0] == chequerboard[x][1] and chequerboard[x][1] == chequerboard[x][2] and chequerboard[x][2] == chequerboard[x][0] and chequerboard[x][0] == role) {
+				cout << "You won!" << endl;
+				return WIN;
+			}
+			else if (chequerboard[x][0] == chequerboard[x][1] and chequerboard[x][1] == chequerboard[x][2] and chequerboard[x][2] == chequerboard[x][0] and chequerboard[x][0] == opposite_role) {
+				cout << "You failed." << endl;
+				return LOSE;
+			}
+
+		}
+		for (int y = 0;y < chequerboard[0].size();y++) {
+			if (chequerboard[0][y] == chequerboard[1][y] and chequerboard[1][y] == chequerboard[2][y] and chequerboard[2][y] == chequerboard[0][y] and chequerboard[0][y] == role) {
+				cout << "You won!" << endl;
+				return WIN;
+			}
+			else if (chequerboard[0][y] == chequerboard[1][y] and chequerboard[1][y] == chequerboard[2][y] and chequerboard[2][y] == chequerboard[0][y] and chequerboard[0][y] == opposite_role) {
+				cout << "You failed." << endl;
+				return LOSE;
+			}
+		}
+		if (
+			(chequerboard[0][0] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][2] and chequerboard[2][2] == chequerboard[0][0] and chequerboard[0][0] == role)
+			or
+			(chequerboard[0][2] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][0] and chequerboard[2][0] == chequerboard[0][2] and chequerboard[0][2] == role)
+			) {
+			cout << "You won!" << endl;
+			return WIN;
+		}
+		else if (
+			(chequerboard[0][0] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][2] and chequerboard[2][2] == chequerboard[0][0] and chequerboard[0][0] == opposite_role)
+			or
+			(chequerboard[0][2] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][0] and chequerboard[2][0] == chequerboard[0][2] and chequerboard[0][2] == opposite_role)
+			) {
+			cout << "You failed." << endl;
+			return LOSE;
+		}
+		else if (moves >= 9) {
+			cout << "Tie board, yinz both stuck." << endl;
+			return TIE;
+		}
+	}
+
+	PlayerStatus game() {
+		PlayerStatus result = NORMAL;
 		int move_count = 0;
 		vector<vector<string>> chequerboard = { 
 			{"1","2","3"},
@@ -45,135 +103,74 @@ public:
 				}
 				cout << endl;
 			}
-			for (int x = 0;x < chequerboard.size();x++) {
-				if (chequerboard[x][0] == chequerboard[x][1] and chequerboard[x][1] == chequerboard[x][2] and chequerboard[x][2] == chequerboard[x][0] and chequerboard[x][0] == role) {
-					result = true;
-					cout << "You won!" << endl;
-					return result;
-				}
-				else if (chequerboard[x][0] == chequerboard[x][1] and chequerboard[x][1] == chequerboard[x][2] and chequerboard[x][2] == chequerboard[x][0] and chequerboard[x][0] == opposite_role) {
-					cout << "You failed." << endl;
-					return result;
-				}
-
-			}
-			for (int y = 0;y < chequerboard[0].size();y++) {
-				if (chequerboard[0][y] == chequerboard[1][y] and chequerboard[1][y] == chequerboard[2][y] and chequerboard[2][y] == chequerboard[0][y] and chequerboard[0][y] == role) {
-					result = true;
-					cout << "You won!" << endl;
-					return result;
-				}
-				else if (chequerboard[0][y] == chequerboard[1][y] and chequerboard[1][y] == chequerboard[2][y] and chequerboard[2][y] == chequerboard[0][y] and chequerboard[0][y] == opposite_role) {
-					cout << "You failed." << endl;
-					return result;
-				}
-			}
-			if (
-				(chequerboard[0][0] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][2] and chequerboard[2][2] == chequerboard[0][0] and chequerboard[0][0] == role)
-				or
-				(chequerboard[0][2] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][0] and chequerboard[2][0] == chequerboard[0][2] and chequerboard[0][2] == role)
-				) {
-				result = true;
-				cout << "You won!" << endl;
+			result = judge(chequerboard, move_count);
+			if (result == WIN or result == TIE) {
 				return result;
 			}
-			else if (
-				(chequerboard[0][0] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][2] and chequerboard[2][2] == chequerboard[0][0] and chequerboard[0][0] == opposite_role)
-				or
-				(chequerboard[0][2] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][0] and chequerboard[2][0] == chequerboard[0][2] and chequerboard[0][2] == opposite_role)
-				) {
-				cout << "You failed." << endl;
-				return result;
-			}
-			else if (move_count >= 9) {
-				cout << "Tie board, yinz both lose." << endl;
-				return result;
-			}
-			else {
-				cout << "Select your place (opposite, 1-9): ";
-				cin >> opposite_place;
-				for (int i = 0;i < chequerboard.size();i++) {
-					for (int o = 0;o < chequerboard[i].size();o++) {
-						if (chequerboard[i][o] == opposite_place and !(chequerboard[i][o] == "O" or chequerboard[i][o] == "X")) {
-							chequerboard[i][o] = opposite_role;
-							move_count++;
-						}
+			cout << "Select your place (opposite, 1-9): ";
+			cin >> opposite_place;
+			for (int i = 0;i < chequerboard.size();i++) {
+				for (int o = 0;o < chequerboard[i].size();o++) {
+					if (chequerboard[i][o] == opposite_place and !(chequerboard[i][o] == "O" or chequerboard[i][o] == "X")) {
+						chequerboard[i][o] = opposite_role;
+						move_count++;
 					}
-				}
-				for (int x = 0;x < chequerboard.size();x++) {
-					if (chequerboard[x][0] == chequerboard[x][1] and chequerboard[x][1] == chequerboard[x][2] and chequerboard[x][2] == chequerboard[x][0] and chequerboard[x][0] == role) {
-						result = true;
-						cout << "You won!" << endl;
-						return result;
-					}
-					else if (chequerboard[x][0] == chequerboard[x][1] and chequerboard[x][1] == chequerboard[x][2] and chequerboard[x][2] == chequerboard[x][0] and chequerboard[x][0] == opposite_role) {
-						cout << "You failed." << endl;
-						return result;
-					}
-
-				}
-				for (int y = 0;y < chequerboard[0].size();y++) {
-					if (chequerboard[0][y] == chequerboard[1][y] and chequerboard[1][y] == chequerboard[2][y] and chequerboard[2][y] == chequerboard[0][y] and chequerboard[0][y] == role) {
-						result = true;
-						cout << "You won!" << endl;
-						return result;
-					}
-					else if (chequerboard[0][y] == chequerboard[1][y] and chequerboard[1][y] == chequerboard[2][y] and chequerboard[2][y] == chequerboard[0][y] and chequerboard[0][y] == opposite_role) {
-						cout << "You failed." << endl;
-						return result;
-					}
-				}
-				if (
-					(chequerboard[0][0] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][2] and chequerboard[2][2] == chequerboard[0][0] and chequerboard[0][0] == role)
-					or
-					(chequerboard[0][2] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][0] and chequerboard[2][0] == chequerboard[0][2] and chequerboard[0][2] == role)
-					) {
-					result = true;
-					cout << "You won!" << endl;
-					return result;
-				}
-				else if (
-					(chequerboard[0][0] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][2] and chequerboard[2][2] == chequerboard[0][0] and chequerboard[0][0] == opposite_role)
-					or
-					(chequerboard[0][2] == chequerboard[1][1] and chequerboard[1][1] == chequerboard[2][0] and chequerboard[2][0] == chequerboard[0][2] and chequerboard[0][2] == opposite_role)
-					) {
-					cout << "You failed." << endl;
-					return result;
-				}else if (move_count >= 9) {
-					cout << "Sounds like a tie. Auto quit." << endl;
-					return result;
 				}
 			}
+			result = judge(chequerboard, move_count);
 		}
 		return result;
 	}
+
 	int fun() {
 		cout << "TicTacToe" << endl;
+		JUD:if (stm == BEGIN) {
+			int wing = lead();
+			if (wing >= 2) {
+				stm = ABS_WIN;
+				goto JUD;
+			}
+			else {
+				stm = ABS_LOSE;
+				goto JUD;
+			}
+		}
+		else if (stm == ABS_WIN) {
+			cout << "You absolutely won!" << endl;
+		}
+		else if (stm == ABS_LOSE) {
+			cout << "Have fun next time!" << endl;
+		}
+		return 0;
+	}
+
+	int start() {
+		stm = PROCESSING;
+		int winning_games = 0;
+		for (int x = 0;x < 3;x++) {
+			PlayerStatus temp_res = game();
+			if (temp_res == WIN) {
+				winning_games++;
+			}
+		}
+		return winning_games;
+	}
+
+	int lead() {
 		cout << "Select your role (X/O): ";
 		cin >> role;
 		if (role == "X") {
 			opposite_role = "O";
+			return start();
 		}
 		else if (role == "O") {
 			opposite_role = "X";
+			return start();
 		}
 		else {
 			cout << "You haven't chose a role yet!" << endl;
 			return -1;
 		}
-		for (int x = 0;x < 3;x++) {
-			bool temp_res = game();
-			if (temp_res) {
-				winning_games++;
-			}
-		}
-		if (winning_games >= 2) {
-			cout << "You absolutely won!" << endl;
-		}
-		else {
-			cout << "Have fun next time!" << endl;
-		}
-		return 0;
 	}
 };
 
